4bbfa887a734e582fd046338aefa806958f9290e
Ngoc Nguyen <nnpk0410@gmail.com>
2023-06-17 15:06:06 -0500

    create Remaining.js and import to App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./component/Navbar";
 import Budget from "./component/Budget";
+import Remaining from "./component/Remaining";
 
 function App() {
   return (
     <div className="container">
         <Navbar />
-        <div className="row mt-3">
-          <div className="col-sm">
+        <div className="col mt-3">
+          <div className="row-sm">
             <Budget />
+          </div>
+          <div className="row-sm">
+            <Remaining />
           </div>
         </div>
     </div>
---
+++ src/component/Remaining.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Remaining() {
+    return (
+        <div className="alert alert-success">
+            <span>Remaining: $1000</span>
+        </div>
+    )
+}
