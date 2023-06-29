7f5896f2395dbd4707582fa6e97d96ed0753be38
Ngoc Nguyen <nnpk0410@gmail.com>
2023-06-29 09:00:10 -0500

    name the app : BudgetWise

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,16 +6,16 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg bg-body-tertiary mt-2">
       <div className="container">
         <Link to="/" className="navbar-brand">
-          Your Budget Manager
+          BudgetWise
         </Link>
-        <ul className="nav flex-column" >
-          <li className="nav-item">
-            <Link to="/" className="nav-link active" aria-current="page">
+        <ul className="nav flex-column">
+          <li className="nav-item active">
+            <Link to="/" className="nav-link">
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/add-expense" className="nav-link">
+            <Link to="/add-expense" className="nav-link ">
               Add Expense
             </Link>
           </li>
